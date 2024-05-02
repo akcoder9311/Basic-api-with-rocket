38861b5302d50c835bc9a623c54efe972bf8d2d2
codekabir <mohdamir787000@gmail.com>
2024-05-02 20:37:33 +0530

    add embedded migrations

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #[macro_use] extern crate rocket;
 extern crate diesel;
 #[macro_use] extern crate rocket_sync_db_pools;
-
+#[macro_use] extern crate diesel_migrations;
 
 
 mod auth;
@@ -9,17 +9,23 @@ mod models;
 mod schema;
 mod repositories;
 
+
 use repositories::RustaceanRepository;
 use models::{Rustacean,NewRustacean};
 use rocket::http::Status;
 use rocket::serde::json::{Value, json,Json};
 use rocket::response::status::{self,Custom};
+use rocket::{Rocket,Build};
+use rocket::fairing::AdHoc;
 use auth::BasicAuth;
+use diesel_migrations::{embed_migrations};
 
 
 
 #[database("sqlite_db")]
 struct DbConn(diesel::SqliteConnection);
+
+embed_migrations!();
 
 #[get("/rustaceans")]
 async fn get_rustaceans(_auth: BasicAuth, db:DbConn) -> Result<Value,Custom<Value>>{
@@ -79,6 +85,23 @@ async fn delete_rustaceans(id:i32, _auth: BasicAuth, db:DbConn )-> Result<status
 fn not_found()->Value{
   json!("cant get anything")
 }
+
+async fn run_db_migrations(rocket: Rocket<Build>)-> Result<Rocket<Build> , Rocket<Build>>{
+
+    DbConn::get_one(&rocket)
+         .await
+         .expect("failed to received database connections")
+         .run(|c| match embedded_migarations::run(c) {
+            Ok(()) => Ok(rocket),
+            Err(e) => {
+               println!("failed to run database migrations : {:? } ",e);
+               Err(rocket)
+            }
+             
+         }).await
+   
+}
+
 #[rocket::main]
 async fn main(){
   let _ = rocket::build()
@@ -93,6 +116,7 @@ async fn main(){
                    not_found
                 ])
                 .attach(DbConn::fairing())
+                .attach(AdHoc::try_on_ignite("Runnig DB Migration", run_db_migrations))
                 .launch()
                 .await;
 }
